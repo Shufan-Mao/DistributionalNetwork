fdd49a90274d8a4d013b2adee296d1b4e0171881
Shufan-Mao <46933875+Shufan-Mao@users.noreply.github.com>
2022-12-26 18:31:04 -0600

    add corpus

--- src/corpus.py
+++ src/corpus.py
@@ -1,1 +1,14 @@
 import nltk
+from missingadjunct.corpus import Corpus
+
+corpus = Corpus(include_location=True,
+                include_location_specific_agents=False,
+                seed=1,
+                num_epochs=1,
+                )
+
+for sentence in corpus.get_sentences():
+    print(sentence)
+
+
+
